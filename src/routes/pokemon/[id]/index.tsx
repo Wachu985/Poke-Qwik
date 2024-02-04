b7e8ba0c69836e6c04f8f1032089c85bb4557115
Wachu985 <pedrobonilla985@gmail.com>
2024-02-03 23:00:24 -0500

    feat: Create routes for project

---
+++ src/routes/pokemon/[id]/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+    return (
+        <>
+            <span class="text-5xl">Pokemon: </span>
+        </>
+    )
+});
---
+++ src/routes/pokemons/list-client/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+    return (
+        <>
+            <h1>Hola Mundo - List Client</h1>
+        </>
+    )
+});
