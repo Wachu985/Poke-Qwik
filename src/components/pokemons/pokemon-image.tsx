033f51416fec8cf654fa74139fddf97af6ed1adf
Wachu985 <pedrobonilla985@gmail.com>
2024-02-03 21:14:55 -0500

    feat: Finished Section 2

---
+++ src/components/pokemons/pokemon-image.tsx
@@ -0,0 +1,39 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+
+interface Props {
+    id: number;
+    size?: number;
+    backImage?: boolean;
+    isVisible?: boolean;
+}
+
+
+export const PokemonImage = component$(({ id, size = 200, backImage = false, isVisible = true }: Props) => {
+
+    const imageLoaded = useSignal<boolean>(false);
+    useTask$(({ track }) => {
+        track(() => id);
+        imageLoaded.value = false;
+    })
+
+
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon${backImage ? "/back" : ""}/${id}.png`;
+    return (
+        <div class="flex items-center justify-center" style={{ width: `${size}px`, height: `${size}px` }}>
+            {!imageLoaded.value && (<span>Cargando....</span>)}
+            <img width="96" height="96"
+                src={imageUrl}
+                alt="Pokemon Sprite"
+                style={{ width: `${size}px` }}
+                onLoad$={() => {
+                    imageLoaded.value = true
+                }}
+                class={{
+                    "hidden": !imageLoaded.value,
+                    'brightness-0': isVisible,
+                    'transition-all': true
+                }}
+            />
+        </div>
+    )
+})
---
+++ src/routes/index.tsx
@@ -0,0 +1,45 @@
+import { component$, useSignal, $ } from '@builder.io/qwik';
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { PokemonImage } from '~/components/pokemons/pokemon-image';
+
+export default component$(() => {
+
+  const pokemonId = useSignal<number>(1); //primitivos , booleans, strings, numbers
+  const showBackImage = useSignal<boolean>(false)
+  const showPokemon = useSignal<boolean>(true);
+
+  const changePokemonId = $((value: number) => {
+    if ((pokemonId.value + value) <= 0) return;
+    pokemonId.value += value;
+  })
+
+
+  return (
+    <>
+
+      <span class="text-2xl">Buscador Simple</span>
+
+      <span class="text-9xl">{pokemonId.value}</span>
+
+      <PokemonImage id={pokemonId.value} backImage={showBackImage.value} isVisible={showPokemon.value} />
+
+      <div class="mr-2">
+        <button onClick$={() => changePokemonId(-1)} class="btn btn-primary mr-2">Anterior</button>
+        <button onClick$={() => showBackImage.value = !showBackImage.value} class="btn btn-primary mr-2">Voltear</button>
+        <button onClick$={() => showPokemon.value = !showPokemon.value} class="btn btn-primary mr-2">Revelar</button>
+        <button onClick$={() => changePokemonId(+1)} class="btn btn-primary">Siguiente</button>
+      </div>
+
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "PokeQwik",
+  meta: [
+    {
+      name: "description",
+      content: "Esta es mi primera aplicacion en Qwik",
+    },
+  ],
+};
