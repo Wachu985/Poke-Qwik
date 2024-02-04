559e35f1d6ac50ca0ed16eb0e314403c04eae9f3
Wachu985 <pedrobonilla985@gmail.com>
2024-02-03 23:13:33 -0500

    feat: Added links in the header and some metadata

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,24 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import { QwikLogo } from "../../icons/qwik";
 import styles from "./navbar.module.css";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <header class={styles.header}>
       <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
-          <a href="/" title="qwik">
-            <QwikLogo height={50} />
-          </a>
+          <Link href="/"><QwikLogo height={50} /></Link>
         </div>
         <ul>
           <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
-            </a>
+            <Link href="/pokemons/list-ssr/">SSR-List</Link>
+          </li>
+          <li>
+            <Link href="/pokemons/list-client/">CLIENT-List</Link>
           </li>
         </ul>
       </div>
--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
     return (
@@ -7,3 +8,13 @@ export default component$(() => {
         </>
     )
 });
+
+export const head: DocumentHead = {
+    title: `List SSR`,
+    meta: [
+        {
+            name: 'description',
+            content: ' Lista desde el Servidor'
+        }
+    ]
+};
